92edd125c8e7e0b4167911bda46f8bd9293c428d
Jkaufman823 <jkaufman823@gmail.com>
2017-07-09 18:21:33 -0400

    Add files via upload

--- lobbies.php
+++ lobbies.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '/home/nrylee1/public_html/Project2/logging.php';
+   /*require_once '/home/nrylee1/public_html/Project2/logging.php';
     writeToLogWhenDebug('Loading lobby page');
     if(session_start()) {
         writeToLogWhenDebug('Session started successfully.');
@@ -10,10 +10,8 @@
     $pid = $_SESSION['pid'];
     if( ! isset($pid) || empty($pid)) {
         header("Location: /Project2/login.php?error=loginerror");
-    }
-
+    }*/
     
-
     function getGameLobbies() 
     /* Use this to list all current game lobbies. They will need a button to join a game if there is an open seat.
      * I will update the function later to actually pull in real games. If you need it to pull in more information, 
@@ -37,7 +35,39 @@
         );
         return $gameList;
     }
+    foreach ($getGameLobby as $gameid => $playerInfoList) {
+        
+        foreach($lobby as $value){
+           $i = 0;
+            $value=lobby[i];
+                foreach($array as $value){
+                    if($value[0]==-1){
+                        $displayOpenLobby;//a lobby is open, display open game
+                    }
+                }
+            }
+        }
+        
+        for($row=0; $row < 4; $row++){
+           if($lobby[$row][0] == -1){
+            echo "<p><"<div class=\"openGame\"></div>"</p>";
+            echo "<ul>";
+            
+               
+           }
+            }
+        }
+        
+        
+        //list of lobbies
+        //game id arrays
+        //write visibilty part of the page
+        //link to join games
+        //display lobbies
+        //show open games
 
+        # code...
+    }
     function getGameLobby($gameid) 
     /* Use this to build Lobby Page (lobby.php) once player joins game, but is waiting for full set of players.
      * Copy and paste to that page if you need to. 
@@ -62,6 +92,9 @@
         <div>
             <section>
                 <header>Lobby 1</header>
+                <div class="openGame">
+                 <a href="GameResources/play.php?gid=aAyugaL" target="_blank">Join Game!</a>
+                 </div>
                 <a href="GameResources/play.php?gid=aAyugaL" target="_blank">Play!</a>
                 <footer>Currently 0 Players</footer>
             </section>
